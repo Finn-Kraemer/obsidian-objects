7ecf79576eeaa657cfc088309a03a08043897da7
Finn-Kraemer <finn.kraemer@proton.me>
2026-06-01 14:26:12 +0200

    remove notice for missing Templater plugin in verifyIntegrations method

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin, Notice } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { ObsidianObjectsSettings, DEFAULT_SETTINGS } from './types';
 import { SettingsTab } from './settings';
 import { TemplaterHandler } from './templater';
@@ -37,14 +37,9 @@ export default class ObjectsPlugin extends Plugin {
 
     /**
      * Checks if necessary third-party plugins (like Templater) are active.
-     * Displays a notice if Templater is missing.
      */
     private verifyIntegrations() {
         const api = this.templater.getApi();
-        if (!api) {
-            const message = 'Objects: The "Templater" plugin is not active.';
-            new Notice(message, 7000);
-        }
     }
 
     /**
